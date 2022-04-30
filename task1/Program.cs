8c742ccefce581cc9bd3910012ba8bcce6df61b3
Almaz15283 <102859501+Almaz15283@users.noreply.github.com>
2022-04-30 10:36:42 +0300

    task 1 homework

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, User!");
+Console.WriteLine("Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа.");
+int a =new Random().Next(100,999);
+Console.WriteLine(a);
+
+int c = a%100;
+c = c/10;
+Console.Write(" второе число = ");
+Console.Write(c);
+
